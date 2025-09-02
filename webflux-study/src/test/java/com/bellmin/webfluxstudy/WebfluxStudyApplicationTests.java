c98177357ec005ebbdff1b78b4da0a570371469a
Bellmin <whdals011203@naver.com>
2025-09-02 11:59:52 +0900

    chore: 프로젝트 초기 세팅

---
+++ webflux-study/src/test/java/com/bellmin/webfluxstudy/WebfluxStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bellmin.webfluxstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
